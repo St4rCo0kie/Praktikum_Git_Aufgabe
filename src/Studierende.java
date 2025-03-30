ca68c6a47c9794e228332981ff9bda78f09e218f
Asa <ariishi66@gmail.com>
2025-03-30 14:50:43 +0200

    Studierende Improvement, getFrage, setFrage, getName, frageStellen();

--- src/Studierende.java
+++ src/Studierende.java
@@ -2,28 +2,38 @@ public class Studierende {
     private String name;
     private String studiengang;
     private int matrikelnummer;
+    private String frage;
+    private static final String DEFAULT_ANSWER = " Frage erhalten..";
 
-    public void frageStellen() {
-        System.out.println("Der Studierende " + name + " stellt eine Frage an den Dozierenden.");
+    public Studierende(String name, String studiengang, int matrikelnummer) {
+        this.name = name;
+        this.studiengang = studiengang;
+        this.matrikelnummer = matrikelnummer;
+        this.frage = "";
+    }
 
+    public void frageStellen(Dozierender dozierender, String frage) {
+
+        if (frage.isEmpty()) {
+            System.out.println(name + " hat keine Frage gestellt.");
+            return;
+        }
+
+        System.out.println(name + " fragt " + dozierender.getName() + ": " + frage);
+        dozierender.frageBeantworten(name, DEFAULT_ANSWER);
+
+        this.frage = ""; //Clear frage
+    }
+
+    public String getFrage() {
+        return frage;
+    }
+
+    public void setFrage(String frage) {
+        this.frage = frage;
     }
 
     public String getName() {
         return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getStudiengang() {
-        return studiengang;
-    }
-    public void setStudiengang(String studiengang) {
-        this.studiengang = studiengang;
-    }
-    public int getMatrikelnummer() {
-        return matrikelnummer;
-    }
-    public void setMatrikelnummer(int matrikelnummer) {
-        this.matrikelnummer = matrikelnummer;
     }
 }
