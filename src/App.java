16cc61985468f1c7f902c4d58c19bc04740d4932
St4rCo0kie <eva.beck.sob@gmail.com>
2025-04-01 11:38:06 +0200

    Dozierende Klasse

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+         Scanner scanner = new Scanner(System.in);
+
+        Dozierende mustermann = new Dozierende("Herr Musterman", "THI", 123);
+        System.out.println(mustermann.getName() + " ist Dozierender an der " + mustermann.getFakultaet() + " mit der Bueronummer: " + mustermann.getBueroNummer());
+        mustermann.fragenBeantworten(scanner); // () können wir bei Scanner frei lassen, ansonsten müsste hier die Antwort rein
+        
+        scanner.close();
     }
 }
