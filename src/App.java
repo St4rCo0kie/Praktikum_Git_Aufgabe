8c42b00c771392d77d1b5fd2aebac1d99a0df3a0
St4rCo0kie <eva.beck.sob@gmail.com>
2025-04-09 15:02:07 +0200

    Finetuning Dozierender

--- src/App.java
+++ src/App.java
@@ -3,13 +3,19 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         Studierende anna = new Studierende("Anna", "Informatik", 123456);
+        Studierende markus = new Studierende("Markus","Informatik", 654321);
         Dozierende mustermann = new Dozierende("Herr Musterman", "THI", 123);
 
-        System.out.println(mustermann.getName() + " ist Dozierender an der " + mustermann.getFakultaet() + " mit der Bueronummer: " + mustermann.getBueroNummer());
+        //System.out.println(mustermann.getName() + " ist Dozierender an der " + mustermann.getFakultaet() + " mit der Bueronummer: " + mustermann.getBueroNummer());
+        mustermann.infoDozent(); //anstatt langer output
 
-        anna.frageStellen(mustermann,"Mögen sie mich?"); // hier wird die Frage gestellt, die der Student stellen möchte
-        mustermann.frageBeantworten(anna, "NEIN");  // () können wir bei Scanner frei lassen, ansonsten müsste hier die Antwort rein
-        mustermann.Bewertung(anna, 5);
+        anna.frageStellen(mustermann,"Mögen sie mich?");
+        mustermann.frageBeantworten(anna, "NEIN"); 
+        mustermann.Bewertung(anna, 6);
         anna.sprechstundenAnfrage(mustermann, 12, 12, 15);
+        mustermann.Bewertung(anna, 2);
+        mustermann.Bewertung(markus, 3);
+
+        mustermann.printBewertung();
     }
 }
--- src/Dozierende.java
+++ src/Dozierende.java
@@ -3,17 +3,26 @@ public class Dozierende {
     private final String dNamen;
     private final String fakultaet;
     private final int bueroNummer;
-    int gesamtBewertung;
+    private int gesamtBewertung;
+    private int anzahlBewertungen = 0;
+    private int durchschnittBewertung = 0;
 
     public Dozierende(String dNamen, String fakultaet, int bueroNummer) {
         this.dNamen = dNamen;
         this.fakultaet = fakultaet;
         this.bueroNummer = bueroNummer;
-        this.gesamtBewertung = 3;
+        this.gesamtBewertung = 0;
 
     }
     
-    public String getName(){               // getter falls wir die Dozenten variablen in Main oder Studenten brauchen 
+    public void infoDozent(){
+        System.out.println("--------------------");
+        System.out.println("Dozent: " + dNamen);
+        System.out.println("Fakultaet: " + fakultaet);
+        System.out.println("Büro Nummer: " + bueroNummer);
+    }    
+
+    public String getName(){               
         return dNamen;
     }
     public String getFakultaet(){
@@ -23,18 +32,29 @@ public class Dozierende {
         return bueroNummer;
     }
 
-    public void frageBeantworten(Studierende student, String antwort) { // hier wird die Frage beantwortet, die der Student gestellt hat
-        System.out.println(dNamen + " antwortet " + student.getName() + ": " + antwort);  // hier wird die Antwort ausgegeben, die der Dozent gegeben ha
-    } // die outputs sind halt jetzt sehr aus der sich meines dozenten als user, müssen wir uns noch zusammen hocken und schauen wie es zusammen geht
+    public void frageBeantworten(Studierende student, String antwort) {
+        System.out.println("--------------------");
+        System.out.println(dNamen + " antwortet " + student.getName() + ": " + antwort);  
+    }
 
-    public int Bewertung(Studierende studierende, int bewertung) { // hier wird die Bewertung beantwortet, die der Dozent gestellt hat
+    public void Bewertung(Studierende studierende, int bewertung) { 
         if (bewertung < 1 || bewertung > 6) {
+            System.out.println("--------------------");
             System.out.println("Die Bewertung muss zwischen 1 und 6 sein.");
         } else {
-            bewertung =  (this.gesamtBewertung + bewertung) / 2;
-            this.gesamtBewertung = bewertung;
-            System.out.println("Sie haben eine Bewertung abgegeben. Der neue durchschnitt ist: " + bewertung);
+                gesamtBewertung = bewertung + gesamtBewertung;
+                anzahlBewertungen++;
+                durchschnittBewertung = gesamtBewertung / anzahlBewertungen;
+                System.out.println("--------------------");
+                System.out.println("Danke für das Feedback, " + studierende.getName() + "!");
         }
-        return bewertung;
+    }
+
+    public void printBewertung(){
+        System.out.println("--------------------");
+        System.out.println("Dozent: " + dNamen);
+        System.out.println("Anzahl Bewertungen: " + anzahlBewertungen);
+        System.out.println("Durchschnittliche Bewertung: " + durchschnittBewertung);
+
     }
 }
